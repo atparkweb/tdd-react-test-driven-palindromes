96c8bdc42062c8047d3e708f0591e2a06e410d04
A Park <atpark333@gmail.com>
2022-10-18 00:27:36 +0900

    Move tests to their own folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,9 @@
 module.exports = {
-  plugins: ['jest'],
   extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 6,
   },
   env: {
-    "jest/globals": true,
     node: true,
   },
 };
