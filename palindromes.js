bc7cac650b740197e8825a3d772a6fef7891c570
A Park <atpark333@gmail.com>
2022-10-18 12:50:59 +0900

    Add additional tests

--- palindromes.js
+++ palindromes.js
@@ -1,5 +1,13 @@
+const { normalize, reverse } = require('./util');
+
 function palindromes(str) {
-  return [str];
+  let newStr = reverse(normalize(str));
+
+  const result = [];
+  if (newStr === normalize(str)) {
+    result.push(newStr);
+  }
+  return result;
 }
 
 module.exports = palindromes;
